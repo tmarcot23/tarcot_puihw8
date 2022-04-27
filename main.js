e26bcbb6fd7fa19c75de67f13a1bdf2fb3738173
tmarcot23 <tiameghana@gmail.com>
2022-04-27 06:11:40 -0400

    added files and made changes

--- main.js
+++ main.js
@@ -3,15 +3,18 @@ $(document).ready(function(){
     var facts =[
     {
         "image": "assets/skydiving.jpg",
-        "fact":"&emsp;I have been skydiving before<br>&emsp;(at 14,000 feet!)"
+        "fact":"&emsp;I have been skydiving before<br>&emsp;(at 14,000 feet!)",
+        "caption": "Picture of Tia skydiving"
     },
     {
         "image": "assets/crochet.jpg",
-        "fact":"&emsp;I love to crochet and knit"
+        "fact":"&emsp;I love to crochet and knit!",
+        "caption": "Picture of Tia's crocheted scarf"
     },
     {
         "image": "assets/piano.jpg",
-        "fact":"&emsp;I have been playing the piano<br>&emsp;since six years old"
+        "fact":"&emsp;I play the piano, with my<br>&emsp;favorite piece being<br>&emsp;<i>Stickerbrush Symphony</i> from<br>&emsp;<i>Donkey Kong Country 2</i>",
+        "caption": "Picture of Tia playing the piano"
     },
 /*    {
         "image": "assets/piano.jpg",
@@ -22,6 +25,23 @@ $(document).ready(function(){
         i = (i+1)%facts.length;
         console.log(facts[i]);
         $(".changingImage").attr('src', facts[i].image);
+        $(".changingImage").attr('alt', facts[i].caption);
         $(".changingFact").html(facts[i].fact);
+    });
+});
+
+$(document).ready(function(){
+    $('.backToTop').hide();
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 300) {
+            $('.backToTop').fadeIn();
+        } else {
+            $('.backToTop').fadeOut();
+        }
+    });
+    $(".backToTop").on("click", function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 50);
     });
 });
